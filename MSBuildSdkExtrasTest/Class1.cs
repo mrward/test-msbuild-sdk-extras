d9b0da0349c593196c818ba32e7ee84fd9f404c8
Matt Ward <matt.ward@microsoft.com>
2019-08-01 17:43:10 +0100

    Initial revision

---
+++ MSBuildSdkExtrasTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MSBuildSdkExtrasTest.Android;
+
+namespace MSBuildSdkExtrasTest
+{
+	public class Class1
+	{
+#if TESTANDROID
+		// TESTANDROID is defined in the Directory.Build.targets and should
+		// be defined here.
+		int test;
+#endif
+
+		public void Test ()
+		{
+			// The AndroidTest class should be available but it is not.
+			var test = new AndroidTest ();
+
+		}
+	}
+}
---
+++ MSBuildSdkExtrasTest/Resources/Resource.designer.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+[assembly: global::Android.Runtime.ResourceDesignerAttribute ("MSBuildSdkExtrasTest.Android.Resource", IsApplication = false)]
+
+namespace MSBuildSdkExtrasTest.Android
+{
+	[System.CodeDom.Compiler.GeneratedCodeAttribute ("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	public partial class Resource
+	{
+
+		static Resource ()
+		{
+			global::Android.Runtime.ResourceIdManager.UpdateIdValues ();
+		}
+	}
+}
